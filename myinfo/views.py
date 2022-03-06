011a1b944dd10f24a89b0e2659a1dea9548a23fa
oppe5388 <oppe5388@yahoo.co.jp>
2022-03-06 11:41:29 +0900

    ペーネーション10件

--- myinfo/views.py
+++ myinfo/views.py
@@ -314,7 +314,7 @@ def information_list(request):
     else:
         searchForm = SearchForm()
         informations = Information.objects.all().order_by('-id')
-        page_obj = paginate_queryset(request, informations, 3)#ページネーション用
+        page_obj = paginate_queryset(request, informations, 10)#ページネーション用
         context['page_obj'] = page_obj
         context['informations'] = page_obj.object_list
 
