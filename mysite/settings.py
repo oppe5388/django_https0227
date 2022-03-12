bc1bfb624ae0526f723f522f92d0219b263afe63
oppe5388 <oppe5388@yahoo.co.jp>
2022-03-13 01:42:16 +0900

    環境変数の読込の微調整

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,10 +13,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+# DEBUG = os.environ.get("DEBUG")
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
+# ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
 
 # Application definition
 
