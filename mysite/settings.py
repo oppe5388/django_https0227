d058342b132b162d23d10625a869182587ec21e0
oppe5388 <oppe5388@yahoo.co.jp>
2022-03-14 13:05:31 +0900

    loggingエラーなので初期戻し

--- mysite/settings.py
+++ mysite/settings.py
@@ -185,36 +185,20 @@ SUMMERNOTE_CONFIG = {
 }
 
 
-#とりあえずログ出力設定　https://is.gd/8GCTgJ
+#とりあえずログ出力設定　https://is.gd/8GCTgJ　エラーなので一旦中止
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'production': {
-            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
-                      '%(pathname)s:%(lineno)d %(message)s'
-        },
-    },
     'handlers': {
-        'file': {
-            'class': 'logging.FileHandler',
-            'filename': 'logs/django.log',  #環境に合わせて変更
-            'formatter': 'production',
-            'level': 'INFO',
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
         },
     },
     'loggers': {
-        # 自作したログ出力
-        '': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        # Djangoの警告・エラー
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
         },
     },
 }
