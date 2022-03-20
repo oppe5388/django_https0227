740f9c8a134a9a8a778084629bb9b44508994232
oppe5388 <oppe5388@yahoo.co.jp>
2022-03-20 16:13:41 +0900

    volumeをconsistentに変更とmyschedの不要コード削除

--- mysched/forms.py
+++ mysched/forms.py
@@ -1,20 +1,8 @@
 from django import forms
 from .models import MoneyTrans
-from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import get_user_model
-from datetime import date, datetime
-
-# #メイン
-# class MoneyForm(LoginRequiredMixin, forms.ModelForm):
-
-# class BookForm(ModelForm):
-#     """書籍のフォーム"""
-#     class Meta:
-#         model = Book
-#         fields = ('name', 'publisher', 'page', )
 
 
-#検索
+#カレンダー選択→表示用
 class MoneyForm(LoginRequiredMixin, forms.Form):
-    input_date = forms.DateField(widget=forms.DateInput(attrs={"type":"date"})
+    input_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
--- mysched/views.py
+++ mysched/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404, redirect
-from django.http import HttpResponse, Http404 # 追記
+from django.http import HttpResponse, Http404
 from .models import MoneyTrans
 from .forms import MoneyForm
 from datetime import date, datetime
 
 
 def mysched(request):
-
     moneyForm = MoneyForm(request.GET)
-
     context = {
         'moneyForm': MoneyForm,
     }
 
+    #到着期日が今日以降のデータすべて表示用
+    moneytrans = MoneyTrans.objects.filter(deadline__gte=date.today()).order_by('deadline')
+    context['moneytrans'] = moneytrans
+
+    #登録日をカレンダーで選択→表示用
     if moneyForm.is_valid():
         input_date = moneyForm.cleaned_data['input_date']
+        # request.session['form_data'] = request.GET
         
         if input_date:
             #入力日以降のクエリセットの1つ目
@@ -22,35 +26,10 @@ def mysched(request):
             context['past_sched'] = past_sched
             context['input_date'] = input_date
 
-    else:
-        moneyForm = MoneyForm()
-        #今日以降のクエリセット
-        moneytrans = MoneyTrans.objects.filter(deadline__gte=date.today()).order_by('deadline')
-
-        context['moneytrans'] = moneytrans
-
-    # #通知（有無だけ）
-    # if request.user.id is not None:
-    #     notifi_exis =  Notifications.objects.filter(user=request.user).exists()
-
-    #     #既読も
-    #     # my_unread = ReadStates.objects.all().filter(user=request.user)
-    #     unread_set = ReadStates.objects.all()
-
-    #     #contextは辞書型、context['weather'] = '晴れ'、でkeyがweather、valueが晴れというデータを追加
-    #     context['notifi_exis'] = notifi_exis
-    #     context['unread_set'] = unread_set
-
-
     return render(request, "mysched/mysched.html", context)
-
 
 
     # Model.objects.filter(field__gt='value') #大きい場合
     # model.objects.filter(field__gte='value') #以上の場合
     # Model.objects.filter(field__lt='value') #未満の場合
     # model.objects.filter(field__lte='value') #以下の場合
-
-
-    #入力日が、entryの日以上のQueryセットをとって、その1番上
-    # p = Article.objects.order_by('title', 'pub_date').first()
